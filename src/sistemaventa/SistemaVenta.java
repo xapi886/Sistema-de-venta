685446dec56185ca411554b40f84b1aa6a84379d
xapi08 <xochilt.piche152@std.uni.edu.ni>
2022-05-23 21:11:09 -0600

    first commit

---
+++ src/sistemaventa/SistemaVenta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemaventa;
+
+/**
+ *
+ * @author xapi008
+ */
+public class SistemaVenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
